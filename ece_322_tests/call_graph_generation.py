1d09d746b6eb92ee29a0e53640971e6fee49ffa2
Nathan Klapstein <nklapste@ualberta.ca>
2018-12-01 12:57:36 -0700

    updating reports and report generation

--- ece_322_tests/call_graph_generation.py
+++ ece_322_tests/call_graph_generation.py
@@ -9,49 +9,58 @@ config.debug = True
 config.verbose = True
 
 
-output_parse = GraphvizOutput()
-output_parse.output_file = "pendulum_parse_call_graph.png"
-with PyCallGraph(output=output_parse, config=config):
+output = GraphvizOutput()
+output.output_file = "pendulum_parse_call_graph.png"
+output.output_type = "png"
+with PyCallGraph(output=output, config=config):
     date = pendulum.parse("2018-01-01")
 
 
-output_parse = GraphvizOutput()
-output_parse.output_file = "pendulum_parse_bad_call_graph.png"
-with PyCallGraph(output=output_parse, config=config):
+output = GraphvizOutput()
+output.output_file = "pendulum_parse_bad_call_graph.png"
+output.output_type = "png"
+with PyCallGraph(output=output, config=config):
     try:
         date = pendulum.parse("15512-412")
     except Exception:
         pass
 
 
-output_parse = GraphvizOutput()
-output_parse.output_file = "pendulum_parse_strict_bad_call_graph.png"
-with PyCallGraph(output=output_parse, config=config):
+output = GraphvizOutput()
+output.output_file = "pendulum_parse_strict_bad_call_graph.png"
+output.output_type = "png"
+with PyCallGraph(output=output, config=config):
     try:
         date = pendulum.parse("18-01-01", strict=True)
     except Exception:
         pass
 
 
-output_parse = GraphvizOutput()
-output_parse.output_file = "pendulum_parse_strict_false_call_graph.png"
-with PyCallGraph(output=output_parse, config=config):
+output = GraphvizOutput()
+output.output_file = "pendulum_parse_strict_false_call_graph.png"
+output.output_type = "png"
+with PyCallGraph(output=output, config=config):
     date = pendulum.parse("18-01-01", strict=False)
 
 
-output_now = GraphvizOutput()
-output_now.output_file = "pendulum_now_call_graph.png"
-with PyCallGraph(output=output_now, config=config):
+output = GraphvizOutput()
+output.output_file = "pendulum_now_call_graph.png"
+output.output_type = "png"
+with PyCallGraph(output=output, config=config):
     date = pendulum.now()
 
 
-output_duration = GraphvizOutput()
-output_duration.output_file = "pendulum_duration_simple_call_graph.png"
-with PyCallGraph(output=output_duration, config=config):
+output = GraphvizOutput()
+output.output_file = "pendulum_duration_simple_call_graph.png"
+output.output_type = "png"
+with PyCallGraph(output=output, config=config):
     date = pendulum.duration()
 
 
-output_duration = GraphvizOutput()
-output_duration.output_file = "pendulum_duration_adv_call_graph.png"
-with PyCallGraph(output=output_duration, config=config):
-    date = pendulum.duration(days=5)
+output = GraphvizOutput()
+output.output_file = "pendulum_duration_in_words_call_graph.png"
+output.output_type = "png"
+with PyCallGraph(output=output, config=config):
+    dur = pendulum.duration()
+    dur.in_words()
+
